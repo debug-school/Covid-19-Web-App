71840a45d620e0effdb6b478acf3e81a7a917049
Rasel Rana <debug.school404@gmail.com>
2020-10-23 00:53:32 +0600

    Updates

---
+++ precache-manifest.3eaeaf42140ad8cfc1997185b3bae56b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b100062ea8851daeef3b753fe78274da",
+    "url": "/Covid'19%20Web%20App/index.html"
+  },
+  {
+    "revision": "1517b2005434e16368a1",
+    "url": "/Covid'19%20Web%20App/static/css/main.c901b2ce.chunk.css"
+  },
+  {
+    "revision": "9ed83497c57e71d0e998",
+    "url": "/Covid'19%20Web%20App/static/js/2.e27b6ca3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Covid'19%20Web%20App/static/js/2.e27b6ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1517b2005434e16368a1",
+    "url": "/Covid'19%20Web%20App/static/js/main.672a658e.chunk.js"
+  },
+  {
+    "revision": "82021b1b9df1eb92f36a",
+    "url": "/Covid'19%20Web%20App/static/js/runtime-main.8679fff6.js"
+  }
+]);
